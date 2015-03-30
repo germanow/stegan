79ee9bce6aa6f0a1e1e1f6dce77cf202c4b34025
Ivan <iwan.germanow@gmail.com>
2015-03-30 19:24:26 +0300

    Улучшена функция декодирования
    Декодирование происходит по частям

--- lsb.py
+++ lsb.py
@@ -1,5 +1,5 @@
-"""Модуль для кодирования сообщения(utf-8)  
-В последних 2 битах последовательности байтов(имеется виду строка b'...')
+"""Модуль для кодирования и декодирования сообщения(utf-8) 
+В/из последних 2 битах последовательности байтов(имеется виду строка b'...')
 Используется только стандартная библиотека
 """
 
@@ -39,21 +39,17 @@ def message_encode(container, text):
 		end = []
 		for i in range(0,len(end_mark),2):
 			end.append(end_mark[i]+end_mark[i+1])
-			
 		list.extend(end)
 		return list
 	
-	#Начало работы функции
-	if not container or not text:
+	if (not container) or (not text):
 		raise ValueError("Пустые аргументы")
-	elif type(container) != bytes:
-		raise TypeError("Контейнер должен быть строкой байтов")
 	
 	container = bytearray(container)
 	code = message_to_list_of_bits(text)
 	
 	if len(container) < len(code):
-		raise ValueError("Сообщение не умещается")
+		raise RuntimeError("Сообщение не умещается")
 		
 	for i in range(0,len(code)):
 		container[i] = add_to_byte(container[i],code[i])
@@ -65,27 +61,80 @@ def message_decode(text_container):
 	#Принимает строку байтов и извлекает сообщение 
 	#из 2 последних битов в байтах
 	
-	if not text_container:
-		raise ValueError("Пустой контейнер")
-	elif type(text_container) != bytes:
-		raise TypeError("Контейнер должен быть строкой байтов")
+	def recive_byte(container,number):
+	#Извлекает байт под номером = number из контейнера
 	
-	container = ''
-	for byte in text_container:
-		byte = str(bin(byte))
-		byte = byte.replace('0b','0b'+'0'*(10-len(byte)))
-		container += byte
-
-	message_list = []
-	for i in range(0, len(container), 40):
+		i = number*40
 		byte = (container[i+8:i+10] + container[i+18:i+20] +
 						container[i+28:i+30] + container[i+38:i+40])
 		byte = eval('0b'+byte)
-		#Ищем end_mark 0xff
-		if byte == 255:break
-		message_list.append(byte)
+		if byte == 255:#Ищем end_mark
+			raise RuntimeError
+		return byte
+		
+	def recive_container(text_container,number):
+	#Извлекает контейнер с зашифрованым байтом под номером = number
+	#В виде обычной строки с двоичным представлением байтов(длиной 10)
+
+		number = number*4
+		container = ''
+		for i in range(number,number+4):
+			byte = str(bin(text_container[i]))
+			byte = byte.replace('0b','0b'+'0'*(10-len(byte)))
+			container += byte
+		return container
+	
+	if not text_container:
+		raise ValueError("Пустой контейнер")
+	i = 0
+	container = ''
+	message = ''
+	while i < (len(text_container)/4):
+		try:
+			container += recive_container(text_container,i)#Получили часть контейнера
+			byte = recive_byte(container,i)#Получили закодированный байт из контейнера
+			if 0x00 <= byte <= 0x7f:#Однобайтовый символ
+				message += bytearray([byte]).decode()
+				i+=1
+			elif 0xc0 <= byte <= 0xdf:#Двухбайтовый символ
+				message_list = []
+				message_list.append(byte)
+				i+=1
+				container += recive_container(text_container,i)
+				byte = recive_byte(container,i)
+				message_list.append(byte)
+				i+=1
+				message += bytearray(message_list).decode()
+			elif 0xe0 <= byte <= 0xef:#Трёхбайтовый символ
+				message_list = []
+				for j in range(i,i+3):
+					if j == i:
+						byte = recive_byte(container,j)
+						message_list.append(byte)
+						continue
+					container += recive_container(text_container,j)
+					byte = recive_byte(container,j)
+					message_list.append(byte)
+				i+=3
+				message += bytearray(message_list).decode()
+			elif 0xf0 <= byte <= 0xf7:#Четырёхбайтовый символ
+				message_list = []
+				for j in range(i,i+4):
+					if j == i:
+						byte = recive_byte(container,j)
+						message_list.append(byte)
+						continue
+					container += recive_container(text_container,j)
+					byte = recive_byte(container,j)
+					message_list.append(byte)
+				i+=4
+				message += bytearray(message_list).decode()
+			else:
+				return "Сообщение не найдено"
+		except RuntimeError:#Нашли end_mark
+			break
+		except UnicodeDecodeError:
+			return "Сообщение не найдено"
 	else:
 		return "Сообщение не найдено"
-		
-	message = bytearray(message_list).decode('utf-8')
 	return message
